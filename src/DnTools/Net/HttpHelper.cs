2c62cb91e1494c0ff4333287d70d2a19e0e513f6
gohopo <gohopo@live.com>
2020-02-08 22:33:09 +0800

    init 发布至github package

---
+++ src/DnTools/Net/HttpHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gohopo.DnTools.Net
+{
+    public static class HttpHelper
+    {
+        public static async Task<string> GetAsync(string url, Dictionary<string, string> headers = null)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+                return await client.GetStringAsync(url);
+            }
+        }
+        public static async Task<string> PostAsync(string url, string body, string contentType = null, Dictionary<string, string> headers = null)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+                using (HttpContent content = new StringContent(body, Encoding.UTF8))
+                {
+                    if (contentType != null)
+                    {
+                        content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+                    }
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+    }
+}
